c394973cd812a4905e6fbed1bc76d749c74419a3
Michael Timbrook <timbrook480@gmail.com>
2016-03-09 16:22:47 -0500

    Stuff

--- ref2XYZ.m
+++ ref2XYZ.m
@@ -2,9 +2,18 @@ function [ X, Y, Z ] = ref2XYZ( refs, cmfs, illum )
 %REF2XYZ Summary of this function goes here
 %   Detailed explanation goes here
 
+cied = loadCIEData;
+ref_lambda = 380:5:780;
 
-k = 100 / ( 100 );
+matches = ismember(cied.lambda, cied.lambda);
 
+k = 100 / sum(cmfs(:,2) .* illum(matches)) * 5;
+k
+
+d = 5;
+X = k * sum(cmfs(matches,1) .* illum(matches) .* refs) * d
+Y = k * sum(cmfs(matches,2) .* illum(matches) .* refs) * d
+Z = k * sum(cmfs(matches,3) .* illum(matches) .* refs) * d
 
 end
 
