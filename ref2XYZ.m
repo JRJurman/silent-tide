cd092a0fba1d523b80c4f16d771de61370d08f9d
Michael Timbrook <timbrook480@gmail.com>
2016-03-09 16:40:13 -0500

    .mat?

--- ref2XYZ.m
+++ ref2XYZ.m
@@ -2,18 +2,28 @@ function [ X, Y, Z ] = ref2XYZ( refs, cmfs, illum )
 %REF2XYZ Summary of this function goes here
 %   Detailed explanation goes here
 
+n_illum = illum / 100;
+
+d = 10;
+
 cied = loadCIEData;
-ref_lambda = 380:5:780;
+ref_lambda = 380:d:730;
 
-matches = ismember(cied.lambda, cied.lambda);
+matches = ismember(cied.lambda, ref_lambda);
 
-k = 100 / sum(cmfs(:,2) .* illum(matches)) * 5;
-k
+k = 100 / sum(cmfs(matches,2) .* n_illum(matches) * d);
 
-d = 5;
-X = k * sum(cmfs(matches,1) .* illum(matches) .* refs) * d
-Y = k * sum(cmfs(matches,2) .* illum(matches) .* refs) * d
-Z = k * sum(cmfs(matches,3) .* illum(matches) .* refs) * d
+figure;
+hold on;
+plot(ref_lambda, cmfs(matches,1))
+plot(ref_lambda, cmfs(matches,2))
+plot(ref_lambda, cmfs(matches,3))
+plot(ref_lambda, n_illum(matches))
+hold off;
+
+X = k * sum(cmfs(matches,1) .* n_illum(matches) .* refs * d);
+Y = k * sum(cmfs(matches,2) .* n_illum(matches) .* refs * d);
+Z = k * sum(cmfs(matches,3) .* n_illum(matches) .* refs * d);
 
 end
 
