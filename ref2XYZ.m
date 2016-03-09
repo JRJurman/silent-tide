1fb5cc9d71989470785a010172ca47767ffc0d84
Jesse Jurman <j.r.jurman@gmail.com>
2016-03-09 17:38:03 -0500

    stuff

--- ref2XYZ.m
+++ ref2XYZ.m
@@ -1,29 +1,22 @@
-function [ X, Y, Z ] = ref2XYZ( refs, cmfs, illum )
+function [ ret ] = ref2XYZ( refs, cmfs, illum )
 %REF2XYZ Summary of this function goes here
 %   Detailed explanation goes here
 
 n_illum = illum / 100;
 
-d = 10;
+d = 5;
 
 cied = loadCIEData;
-ref_lambda = 380:d:730;
+ref_lambda = 380:d:780;
 
 matches = ismember(cied.lambda, ref_lambda);
 
 k = 100 / sum(cmfs(matches,2) .* n_illum(matches) * d);
 
-figure;
-hold on;
-plot(ref_lambda, cmfs(matches,1))
-plot(ref_lambda, cmfs(matches,2))
-plot(ref_lambda, cmfs(matches,3))
-plot(ref_lambda, n_illum(matches))
-hold off;
+X = k * ((cmfs(matches,1) .* n_illum(matches))' * refs * d);
+Y = k * ((cmfs(matches,2) .* n_illum(matches))' * refs * d);
+Z = k * ((cmfs(matches,3) .* n_illum(matches))' * refs * d);
 
-X = k * sum(cmfs(matches,1) .* n_illum(matches) .* refs * d);
-Y = k * sum(cmfs(matches,2) .* n_illum(matches) .* refs * d);
-Z = k * sum(cmfs(matches,3) .* n_illum(matches) .* refs * d);
-
+ret = [X; Y; Z]
 end
 
