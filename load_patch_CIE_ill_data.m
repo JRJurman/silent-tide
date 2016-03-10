117a220fd8bfbb6ef875040e4a54254dc3454ce9
Jesse Jurman <j.r.jurman@gmail.com>
2016-03-10 12:19:18 -0500

    smaller lambdas

--- load_patch_CIE_ill_data.m
+++ load_patch_CIE_ill_data.m
@@ -2,7 +2,7 @@
 
 % wavelength data
 cie = loadCIEdata;
-ref_lambda = 380:10:730;
+ref_lambda = 410:10:730;
 % grab only the lambdas that we are using
 matching = ismember(cie.lambda, ref_lambda);
 
@@ -12,20 +12,20 @@ st_imaged = import_sp('colormonki/mac/mac_silent_tide_screen.sp') / 100;
 st_matching = import_sp('colormonki/mac/mac_silent_tide_matching.sp') / 100;
 
 % convert from refs to XYZ to xyY
-st_real_XYZ = ref2XYZ(st_real', cie.cmf2deg(matching), cie.illD65(matching));
+st_real_XYZ = ref2XYZ(st_real(4:end)', cie.cmf2deg(matching), cie.illD65(matching));
 st_real_xyY = XYZ2xyY(st_real_XYZ);
 
-st_imaged_XYZ = ref2XYZ(st_imaged', cie.cmf2deg(matching), cie.illD65(matching));
+st_imaged_XYZ = ref2XYZ(st_imaged(4:end)', cie.cmf2deg(matching), cie.illD65(matching));
 st_imaged_xyY = XYZ2xyY(st_imaged_XYZ);
 
-st_matching_XYZ = ref2XYZ(st_matching', cie.cmf2deg(matching), cie.illD65(matching));
+st_matching_XYZ = ref2XYZ(st_matching(4:end)', cie.cmf2deg(matching), cie.illD65(matching));
 st_matching_xyY = XYZ2xyY(st_matching_XYZ);
 
 %% teal mosaic processing
 
 % wavelength data
 cie = loadCIEdata;
-ref_lambda = 380:10:730;
+ref_lambda = 410:10:730;
 % grab only the lambdas that we are using
 matching = ismember(cie.lambda, ref_lambda);
 
@@ -35,11 +35,11 @@ tm_matching = import_sp('colormonki/mac/mac_teal_mosaic_matching.sp') / 100;
 tm_imaged = import_sp('colormonki/mac/mac_teal_mosaic_screen.sp') / 100;
 
 % convert from refs to XYZ to xyY
-tm_real_XYZ = ref2XYZ(tm_real', cie.cmf2deg(matching), cie.illD65(matching));
+tm_real_XYZ = ref2XYZ(tm_real(4:end)', cie.cmf2deg(matching), cie.illD65(matching));
 tm_real_xyY = XYZ2xyY(tm_real_XYZ);
 
-tm_imaged_XYZ = ref2XYZ(tm_imaged', cie.cmf2deg(matching), cie.illD65(matching));
+tm_imaged_XYZ = ref2XYZ(tm_imaged(4:end)', cie.cmf2deg(matching), cie.illD65(matching));
 tm_imaged_xyY = XYZ2xyY(tm_imaged_XYZ);
 
-tm_matching_XYZ = ref2XYZ(tm_matching', cie.cmf2deg(matching), cie.illD65(matching));
+tm_matching_XYZ = ref2XYZ(tm_matching(4:end)', cie.cmf2deg(matching), cie.illD65(matching));
 tm_matching_xyY = XYZ2xyY(tm_matching_XYZ);
