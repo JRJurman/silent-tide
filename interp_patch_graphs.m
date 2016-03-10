24efbe913064c04ae3e157a3479262a4aa314322
Michael Timbrook <timbrook480@gmail.com>
2016-03-10 12:36:33 -0500

    Jesse's ok I guess

--- interp_patch_graphs.m
+++ interp_patch_graphs.m
@@ -1,8 +1,12 @@
-% generate graphs for the interpolated data
-load_patch_CIE_ill_data
+%% Silent Tide Graph
 
+load_patch_CIE_ill_data
 figure;
 hold on;
+
+title('patch 27.1 measured and interpolated specta');
+xlabel('wavelength (nm)');
+ylabel('relectance factor');
 
 plot(ref_lambda, st_real, 'bo');
 plot(ref_lambda, st_imaged, 'ro');
@@ -13,3 +17,24 @@ plot(cie.lambda, interp1(ref_lambda, st_imaged, cie.lambda, 'linear','extrap'), 
 plot(cie.lambda, interp1(ref_lambda, st_matching, cie.lambda, 'linear','extrap'), 'k.', 'MarkerSize', 15);
 
 axis([350,800,0.0,1.0]);
+legend('real measured','imaged measured','matched measured','real interpolated','imaged interpolated','matched interpolated','Location','NorthEast');
+
+%% Teal Mosaic Graph
+load_patch_CIE_ill_data
+figure;
+hold on;
+
+title('patch 27.2 measured and interpolated specta');
+xlabel('wavelength (nm)');
+ylabel('relectance factor');
+
+plot(ref_lambda, tm_real, 'bo');
+plot(ref_lambda, tm_imaged, 'ro');
+plot(ref_lambda, tm_matching, 'go');
+
+plot(cie.lambda, interp1(ref_lambda, tm_real, cie.lambda, 'linear','extrap'), 'k.', 'MarkerSize', 15);
+plot(cie.lambda, interp1(ref_lambda, tm_imaged, cie.lambda, 'linear','extrap'), 'k.', 'MarkerSize', 15);
+plot(cie.lambda, interp1(ref_lambda, tm_matching, cie.lambda, 'linear','extrap'), 'k.', 'MarkerSize', 15);
+
+axis([350,800,0.0,1.0]);
+legend('real measured','imaged measured','matched measured','real interpolated','imaged interpolated','matched interpolated','Location','NorthEast');
