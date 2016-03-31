fe009e5ce4cde0b073c0457c7f8a7efb8e4ccbe0
Michael Timbrook <timbrook480@gmail.com>
2016-03-31 17:40:04 -0400

    look jesse it works, it works jesse

--- ColorChecker_imaged_values.m
+++ ColorChecker_imaged_values.m
@@ -7,22 +7,24 @@ width = 350;
 xs = linspace(258,2682, 6) - (width / 2);
 ys = linspace(245, 1675, 4) - (width / 2);
 
-[x, y] = meshgrid(xs, ys);
+[x, y] = meshgrid(ys, xs); % row col, x y
 
 rx = reshape(x, [24, 1]);
 ry = reshape(y, [24, 1]);
 
 means = zeros([24 3]);
-for idx = 1:16
-    crop_squares = color_patch_crop(x(idx):x(idx) + width, y(idx):y(idx) + width, :);
+for idx = 1:24
+    crop_squares = color_patch_crop(rx(idx):rx(idx) + width, ry(idx):ry(idx) + width, 1:3);
     [r, c, d] = size(crop_squares);
     flattened = reshape(crop_squares, [r * c, d]);
     mean_rgb = mean(flattened);
     norm_mean = mean_rgb ./ 255;
+%     rectangle('Position', [ rx(idx), ry(idx), width, width ], 'FaceColor', norm_mean);
     means(idx, :) = norm_mean;
 end
 
-image(reshape(means, [4 6 3]));
+ni = reshape(means, [6 4 3]);
+image(fliplr(rot90(ni, 3)));
 
 
 % scatter(rx, ry, 20000, 'square', 'white');
