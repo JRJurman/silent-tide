a7e8c7585645b2e70a7837a2a970fb7627dc2bd7
Jesse Jurman <jrjurman@gmail.com>
2016-03-31 01:32:34 -0400

    fix name bug; better tabs (must be OS dep)

--- ColorChecker_CIELAB.m
+++ ColorChecker_CIELAB.m
@@ -17,8 +17,8 @@ numberOfPatches = nameSize(1);
 
 % print the patch data
 fprintf('ColorChecker XYZ and Lab values (D65 illuminant and 2 deg. observer)\n\n');
-fprintf('Patch #\t\tX\t\tY\t\tZ\t\tL*\t\ta*\t\tb*\t\tPatchName\n');
+fprintf('Patch #\tX\tY\tZ\tL*\ta*\tb*\tPatchName\n');
 
 for i = 1:numberOfPatches
-    fprintf('%2.0f\t\t\t%2.3f\t%2.3f\t%2.3f\t%2.3f\t%2.3f\t%2.3f\t%s\n', i, CC_XYZs(1,i), CC_XYZs(2,i), CC_XYZs(3,i), CC_Labs(1,i), CC_Labs(2,i), CC_Labs(3,i), print_names{i});
+    fprintf('%2.0f\t%2.3f\t%2.3f\t%2.3f\t%2.3f\t%2.3f\t%2.3f\t%s\n', i, CC_XYZs(1,i), CC_XYZs(2,i), CC_XYZs(3,i), CC_Labs(1,i), CC_Labs(2,i), CC_Labs(3,i), names{i});
 end
