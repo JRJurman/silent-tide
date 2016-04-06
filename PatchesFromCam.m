07a1b01490070cc58cd4dc1da5682fe786a57ee1
Michael Timbrook <timbrook480@gmail.com>
2016-04-06 15:54:09 -0400

    Patchs from cam`

--- PatchesFromCam.m
+++ PatchesFromCam.m
@@ -5,11 +5,13 @@ hold on;
 subplot(1,2,1);
 
 real_patches(:,1) = st_real_XYZ;
-real_patches(:,2) = tm_real_XYZ;
+real_patches(:,2) = st_real_XYZ;
+real_patches(:,3) = tm_real_XYZ;
+real_patches(:,4) = tm_real_XYZ;
 
 munki_XYZs_D65 = catBradford(real_patches, XYZ_D50, XYZ_D65);
 munki_XYZs_sRGBs = XYZ2sRGB(munki_XYZs_D65);
-pix = reshape(munki_XYZs_sRGBs', [1 2 3]);
+pix = reshape(munki_XYZs_sRGBs', [2 2 3]);
 pix = uint8(pix*255);
 pix = imrotate(pix, -90);
 pix = flip(pix,2);
@@ -17,11 +19,13 @@ imshow(pix);
 title('munki Color Patches XYZs in sRGB');
 
 img_patches(:,1) = st_imaged_XYZ;
-img_patches(:,2) = tm_imaged_XYZ;
+img_patches(:,2) = st_imaged_XYZ;
+img_patches(:,3) = tm_imaged_XYZ;
+img_patches(:,4) = tm_imaged_XYZ;
 
 munki_XYZs_D65 = catBradford(img_patches, XYZ_D50, XYZ_D65);
 munki_XYZs_sRGBs = XYZ2sRGB(munki_XYZs_D65);
-pix = reshape(munki_XYZs_sRGBs', [1 2 3]);
+pix = reshape(munki_XYZs_sRGBs', [2 2 3]);
 pix = uint8(pix*255);
 pix = imrotate(pix, -90);
 pix = flip(pix,2);
