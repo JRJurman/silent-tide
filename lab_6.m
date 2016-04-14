65d58f27d961c5aa154952296c1e7de1553f147c
Michael Timbrook <timbrook480@gmail.com>
2016-04-14 14:39:43 -0400

    lab_6

---
+++ lab_6.m
@@ -0,0 +1,7 @@
+%% Lab 6
+% Created by Michael Timbrook (mpt2360) and Jesse Jurman (jrj2703) 
+
+%% Step 1, 2, and 3
+% Download the project resources provided in myCourses to your working directory
+% Reading were taken off of Michel's Macbook Pro.
+
