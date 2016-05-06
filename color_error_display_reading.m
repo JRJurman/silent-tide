039ecbce6fba5641efedb11117124d33341ca55a
Michael Timbrook <timbrook480@gmail.com>
2016-05-06 13:48:54 -0400

    stuff

---
+++ color_error_display_reading.m
@@ -0,0 +1,21 @@
+cie = loadCIEdata;
+
+disp_XYZs = importdata('disp_model_test.ti3', ' ', 20);
+
+patches_XYZ = disp_XYZs.data(1:24, 5:7);
+
+black_XYZ = mean(disp_XYZs.data(25:27, 5:7));
+white_XYZ = mean(disp_XYZs.data(28:30, 5:7));
+
+black_LAB = XYZ2Lab(black_XYZ, cie.illD50);
+white_LAB = XYZ2Lab(white_XYZ, cie.illD50);
+
+patches_LAB = XYZ2Lab(patches_XYZ, cie.illD50);
+
+munki_data = importdata('munki_CC_XYZs_Labs.txt');
+munki_XYZs = munki_data(:,2:4)';
+
+% calulate Labs
+munki_Lab = XYZ2Lab(munki_XYZs, cie.illD50);
+
+
