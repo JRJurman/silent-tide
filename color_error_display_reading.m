1c84f75b7b8492119ab99dea2008408adc79a00c
Jesse Jurman <j.r.jurman@gmail.com>
2016-05-06 14:00:05 -0400

    transposing stuff

--- color_error_display_reading.m
+++ color_error_display_reading.m
@@ -7,10 +7,10 @@ patches_XYZ = disp_XYZs.data(1:24, 5:7);
 black_XYZ = mean(disp_XYZs.data(25:27, 5:7));
 white_XYZ = mean(disp_XYZs.data(28:30, 5:7));
 
-black_LAB = XYZ2Lab(black_XYZ, cie.illD50);
-white_LAB = XYZ2Lab(white_XYZ, cie.illD50);
+black_LAB = XYZ2Lab(black_XYZ', cie.illD50);
+white_LAB = XYZ2Lab(white_XYZ', cie.illD50);
 
-patches_LAB = XYZ2Lab(patches_XYZ, cie.illD50);
+patches_LAB = XYZ2Lab(patches_XYZ', cie.illD50);
 
 munki_data = importdata('munki_CC_XYZs_Labs.txt');
 munki_XYZs = munki_data(:,2:4)';
--- lab_7.m
+++ lab_7.m
@@ -2,6 +2,8 @@
 % Created by Michael Timbrook (mpt2360) and Jesse Jurman (jrj2703) 
 
 %% Step 1
+% <include>color_error_display.m</include>
+% <include>color_error_display_reading.m</include>
 
 %% Step 2
 %  evaluating the color error of an uncalibrated color imaging workflow
