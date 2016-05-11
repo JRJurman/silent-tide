ffe5bfccd27f6dec7cb414eaae4e40257eccbbdc
Michael Timbrook <timbrook480@gmail.com>
2016-05-11 13:52:04 -0400

    stuff

--- run_image_through_model.m
+++ run_image_through_model.m
@@ -8,20 +8,21 @@ XYZ_D65 = ref2XYZ(cie.illD65, cie.cmf10deg, cie.illD65);
 image = imread('./imgs_timbrook/color_patch_crop.jpeg');
 
 [r, c, d] = size(image);
-image_vector = reshape(image, [3, r*c]) + 1;
+image_vector = double(reshape(image, [3, r*c]) + 1) / 255;
 
-% Run values through fwd LUT and matrix
-fwd_LUT = [RLUT_fwd; GLUT_fwd; BLUT_fwd];
-fwd_LUT(fwd_LUT<0) = 0;
-fwd_LUT(fwd_LUT>1) = 1;
-image_vector_RS = fwd_LUT(image_vector);
-image_vector_RS_h = [image_vector_RS; ones(1, size(image_vector_RS, 2))];
-image_XYZs = M_fwd * image_vector_RS_h * 100;
+% Run values through camera model
+% use the functions to linearize the camera RGB data
+r = 1; g = 2; b = 3;
+rgbs_lin(r,:) = polyval(cam_polys(r,:),image_vector(r,:));
+rgbs_lin(g,:) = polyval(cam_polys(g,:),image_vector(g,:));
+rgbs_lin(b,:) = polyval(cam_polys(b,:),image_vector(b,:));
+
+image_XYZs = cam_matrix * rgbs_lin;
 
 % remove blacks and run through rev model
 adjusted_XYZs = catBradford(image_XYZs, XYZ_D65, XYZ_D50);
 adjusted_XYZs = bsxfun(@minus, adjusted_XYZs', XYZk_disp);
-final_RGB_lin = (adjusted_XYZs * M_disp') / 100;
+final_RGB_lin = (adjusted_XYZs * M_disp');
 scaled_RGB_lin = ceil(final_RGB_lin * 1024);
 scaled_RGB_lin(scaled_RGB_lin<1) = 1;
 
